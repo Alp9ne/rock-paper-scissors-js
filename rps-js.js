05b025e6a14b6f2108b687372dcce0b658614b63
Alp9ne <dystopiaisunavoidable@gmail.com>
2023-04-15 15:42:38 +0800

    refactor round() function to work using eventListeners added to the three buttons
    add determineWinner function which determines whether score.player or
    score.computer is higher, then changes the html element with id
    'overall-winner'. Score is an object, which is useful for entering data
    into and out of an eventListener function.

--- rps-js.js
+++ rps-js.js
@@ -1,3 +1,8 @@
+
+
+const score = { player: 0, computer: 0 };
+
+// maybe you're supposed to put everything in the eventListener function?
 function getComputerChoice() {
     let a = "rock";
     let b = "scissors";
@@ -10,96 +15,78 @@ function getComputerChoice() {
         return b;
     } else if (d == 2) {
         return c;
-    } else {
-        return;
     };
+}
+
+
+
+
+const btn = document.querySelectorAll('#btn');
+
+if (score.computer != 5 && score.player != 5) {
+    btn.forEach((item) => {
+        item.addEventListener('click', (e) => {
+            let computerSelection = getComputerChoice(), playerSelection = `${e.target.className}`;
+            let isWinner; 
+
+            function round(playerSelection, computerSelection) {
+                if (playerSelection === computerSelection) {
+                        isWinner = null;
+                } else if (playerSelection === 'rock' && computerSelection === 'scissors' 
+                        || playerSelection === 'scissors' && computerSelection === 'paper' 
+                        || playerSelection === 'paper' && computerSelection === 'rock') {
+                        
+                        score.player++;
+                        isWinner = true;
+                } else if (playerSelection === 'rock' && computerSelection === 'paper' 
+                        || playerSelection === 'scissors' && computerSelection === 'rock'
+                        || playerSelection === 'paper' && computerSelection === 'scissors') {
+                        
+                        score.computer++;
+                        isWinner = false;
+                }
+                const results = document.querySelector('#results');
+                const msg = document.createElement('div');
+                
+                results.appendChild(msg);
+                if (isWinner == true) {
+                    msg.textContent += 'You Won!';
+                    msg.style.color = 'green';
+                } else if (isWinner == null) {
+                    msg.textContent += 'Tied!';
+                    msg.style.color = 'blue';
+                } else {
+                    msg.textContent += 'You Lost!';
+                    msg.style.color = 'red';
+                }
+                msg.textContent += ' || ' + playerSelection + ' to ' + computerSelection + ' || Current Score: ' + score.player + ' ' + score.computer;
+            }
+        
+            
+            
+
+            if (score.player < 5 && score.computer < 5) {
+                round(playerSelection, computerSelection);
+            } else {
+                
+                function determineWinner({player}, {computer}) {
+                    if (player > computer) {
+                        return 'player';
+                    } else {
+                        return 'computer';
+                    }
+                }
+                    
+                let winner = determineWinner(score, score)
+                    
+                const gameOver = document.querySelector('#overall-winner');
+                gameOver.textContent = `${winner} wins!`;
+                gameOver.style.textTransform = 'uppercase';
+                gameOver.style.fontWeight = 'bold';
+            }
+        })
+    })
     
-}
-
-let computerSelection;
-let playerSelection;
+} 
 
 
-
-let playerScore=0, computerScore=0, tiedRounds = 0;
-
-function round(computerSelection, playerSelection) {
-    computerSelection = getComputerChoice();
-    playerSelection = prompt("Please enter your choice:").toLowerCase; // input is turned into all lowercase
-    if (playerSelection = `rock`) {
-        switch(computerSelection) {
-            case `rock`:
-                alert("Tie!");
-                tiedRounds += 1;
-                break;
-            case `paper`:
-                alert("You win!");
-                playerScore += 1;
-                break;
-            case `scissors`:
-                alert("You lose!");
-                computerScore += 1;
-                break;
-            default:
-                alert("i did something wrong on the code lol");
-        }
-    } else if (playerSelection = `paper`) {
-        switch(computerSelection) {
-            case `paper`:
-                alert("Tie!");
-                tiedRounds += 1;
-                break;
-            case `scissors`:
-                alert("You lose!");
-                computerScore = computerScore + 1;
-                break;
-            case `rock`:
-                alert("You win!");
-                playerScore = playerScore + 1;
-            default:
-                alert("i did something wrong on the code lol");
-        }
-    } else if (playerSelection = `scissors`) {
-        switch(computerSelection) {
-            case `scissors`:
-                alert("Tie!");
-                tiedRounds += 1;
-                break;
-            case `rock`:
-                alert("You lose!");
-                computerScore += 1;
-                break;
-            case `paper`:
-                alert("You win!");
-                playerScore += 1;
-            default:
-                alert("i did something wrong on the code lol");
-        }
-    } else {
-        alert("Did not enter choice.");
-    };
-    console.log(playerScore, computerScore, tiedRounds);
-}
-
-function game() {
-    let i = 0;
-    while (i < 5) {
-        round(computerSelection, playerSelection);
-        i++;
-    };
-    if (computerScore > playerScore || computerScore == 5 - tiedRounds - 1) {
-        alert("You Lose! " + playerScore + " to " + computerScore);
-    } else if (playerScore > computerScore || playerScore == 5 - tiedRounds - 1) {
-        alert("You win! " + playerScore + " to " + computerScore);
-    }
-    else if (playerScore == computerScore) {
-        alert ("Tie!");
-    }
-    console.log(tiedRounds);
-};
-
-
-
-game();
-
-console.log(playerScore, computerScore, tiedRounds);
